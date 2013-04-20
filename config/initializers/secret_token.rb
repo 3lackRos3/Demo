8a03af59e15d8f2d133190df46b2e2ecf88ab83b
Prem <prem@prem-HCL-Notebook.(none)>
2013-04-20 22:32:34 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '4f62e9854beb21bd1b27d3dfa96c326b1cb80e224b3d33cfa8c3e1ba93ce531c826d431fe086bbdfb821f880d80ad508cc18e548c6f6797d57d0ef1e70817c1d'
